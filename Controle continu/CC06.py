c4b4732a4c8ec1c4682bb6c40e871786f6016a95
Nair0fl <boche.florian@outlook.fr>
2019-04-04 09:57:35 +0200

    test

--- Controle continu/CC06.py
+++ Controle continu/CC06.py
@@ -4,14 +4,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 np.random.seed(0)
-x = [[1], [3], [5], [9], [10], [11], [12], [13], [15], [18]]
+x = [[0], [2], [4], [8], [9], [10], [11], [12], [14], [19]]
 y =[[81682.0], [81720.0], [81760.0], [81826.0], [81844.0], [81864.0], [81881.0], [81900.0], [81933.0], [82003.0]]
 
 
 regression_model = LinearRegression()
 regression_model.fit(x, y)
 y_predicted = regression_model.predict(x)
-y_lendement=regression_model.predict([[19]])
+y_lendement=regression_model.predict([[20]])
 
 plt.ylim((81600,82100))
 
@@ -22,5 +22,3 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.plot(x, y_predicted, color='r')
 plt.show()
-
-print
