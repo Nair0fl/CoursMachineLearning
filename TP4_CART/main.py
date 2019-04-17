ea082edccf676acb1e9987ae87358a2f4e7f4ed2
Nair0fl <boche.florian@outlook.fr>
2019-04-17 19:01:19 +0200

    MAJ comparatif

--- TP4_CART/main.py
+++ TP4_CART/main.py
@@ -16,19 +16,20 @@ def main():
     time = timeit.timeit(predictorRF.run, number=10)
     stats = predictorRF.run()
     print(f' Classifier randomforest \n ------------------\n')
-    print(f' Temps {time:.2f} seconds\n')
-    print(stats)
+    print(f' Temps {time:.2f} seconds\n') #En général le classifieur OVR met 17s pour traiter le data set
+    print(stats) # 97% de bonne réponses
+    
     time = timeit.timeit(predictorOVO.run_predict, number=10)
     stats = predictorOVO.run_predict()
     print(f' Classifier ovo \n ------------------\n')
-    print(f' Temps {time:.2f} seconds\n')
-    print(stats)
-    /*time = timeit.timeit(predictorOVR.run_predict, number=10)
+    print(f' Temps {time:.2f} seconds\n')#En général le classifieur OVR met 12s pour traiter le data set
+    print(stats)#98% de bonne réponses
+    time = timeit.timeit(predictorOVR.run_predict, number=10)
     stats = predictorOVR.run_predict()
     print(f' Classifier ovr \n ------------------\n')
-    print(f' Temps {time:.2f} seconds\n')
-    print(stats)
-
+    print(f' Temps {time:.2f} seconds\n')  #En général le classifieur OVR met 5.8s pour traiter le data set
+    print(stats) #sur tout les données 95% de bonne réponses
+    
 
 if __name__ == '__main__':
     main()
